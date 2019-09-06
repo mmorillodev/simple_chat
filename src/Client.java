b8aea02d91b65ddf205d62c0dde121cd4f5f190c
mmorillo <matheus.morillo@nescara.com>
2019-09-06 10:01:34 -0300

    first commit

---
+++ src/Client.java
@@ -0,0 +1,7 @@
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        Socket client = new Socket();
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server implements Runnable {
+    private final List<Socket> clients;
+    private final ServerSocket server;
+
+    public Server() {
+        clients = new ArrayList<>();
+        server = null;
+        try {
+            server = new ServerSocket(8080);
+        } catch(IOException e) {
+            System.err.println("Port already in use!");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Server().main();
+    }
+
+    public void main() {
+        new Thread(this).run();
+    }
+
+    @Override
+    public void run() {
+        try {
+            clients.add(server.accept());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
